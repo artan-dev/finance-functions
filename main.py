91c9d21ce4b78331fdaec49341d4c141e1d59a76
artan-dev <artant.dev@gmail.com>
2024-09-06 20:56:05 -0400

    new file commit

---
+++ main.py
@@ -0,0 +1,14 @@
+""" 
+This script contains various functions that can be used for financial analysis of the stock market.
+
+Other use cases pending...
+
+"""
+import datetime as dt
+import pandas as pd
+import pandas_datareader.data as web
+
+print('hello world')
+
+
+
